c94343bfac250353d241f9392552604caaf1513f
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-13 22:33:00 -0700

    Challenge: updating textview on click

--- app/src/main/java/com/example/kevinwong/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/kevinwong/geoquiz/MainActivity.java
@@ -57,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
